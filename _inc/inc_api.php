2bf790ce5e215544241e8082fe1bc5983fe876bc
Marc Ruef <marc.ruef@computec.ch>
2018-08-08 14:24:23 +0200

    Init

---
+++ _inc/inc_api.php
@@ -0,0 +1,27 @@
+<?php
+
+function vuldb_api($apikey, $postarr, $details=0, $endpoint='https://vuldb.com/?api'){
+	//Add API key
+	$postarr['apikey']	= $apikey;
+	$postarr['details']	= $details;
+
+	//Prepare http query
+	$postdata = http_build_query($postarr);
+
+	//Post data as opts
+	$opts =[ 'http' =>
+		[
+			'method'  => 'POST',
+			'content' => $postdata
+		]
+	];
+
+	//Request from API endpoint
+	$context = stream_context_create($opts);
+	$result = file_get_contents($endpoint, false, $context);
+	
+	return $result;
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+//You need to put your personal API key here
+$my_api_key = '[your_personal_api_key]';
+
+$DIRINCLUDES = '_inc/';
+require $DIRINCLUDES.'inc_api.php';
+
+$api_request		= [ 'recent' => 3, 'details' => 0 ];		//Prepare request
+$api_response_json	= vuldb_api($my_api_key, $api_request);		//Send request and receive json
+$api_response_arr	= json_decode($api_response_json, TRUE);	//Convert json to php variable
+
+//Shot results
+
+echo "\n";
+echo 'API RESPONSE HEADER FIELDS'."\n";
+foreach($api_response_arr['response'] as $k=>$v){
+	echo htmlentities($k).': '.htmlentities($v)."\n";
+}
+echo "\n\n";
+
+echo 'API REQUEST HEADER FIELDS'."\n";
+foreach($api_response_arr['request'] as $k=>$v){
+	echo htmlentities($k).': '.htmlentities($v)."\n";
+}
+echo "\n\n";
+
+echo 'API RESULT DATA'."\n";
+if(is_array($api_response_arr['result'])){
+	htmlentities(print_r($api_response_arr['result']));
+}
+echo "\n\n";
+
+?>
